b0848dd91c244c344fb24df5e5f864cb897bd717
Giannis21kr <Giannis21kr@gmail.com>
2014-11-14 21:31:34 +0000

    Added starter_single.m Fixed starter_crossvalind.m

---
+++ starter_crossvalind.m
@@ -0,0 +1,41 @@
+clc;
+clear all
+close all
+for layer_sizes = 50:100:60
+
+res_sum = 0;
+curr_res = 0;
+% test_array = [];
+for rounds = 1:10
+    pretrain_maxepoch = 10;
+%     numhid=200; numpen=200; numpen2=200; 
+     numhid=layer_sizes; numpen=layer_sizes; numpen2=layer_sizes; 
+
+    finetuning_maxepoch = 2;
+    conjugate_gradient_max_iter = 3;
+
+    curr_set = strcat('/set',num2str(rounds, '%02i'));
+    f_train0 = strcat('normalize_softmax',curr_set,'/train0');
+    f_train1 = strcat('normalize_softmax',curr_set,'/train1');
+    f_test0 =  strcat('normalize_softmax',curr_set,'/test0');
+    f_test1 = strcat('normalize_softmax',curr_set,'/test1');
+
+    fprintf(1,'Round:%i \n',rounds);
+
+    classify;
+    close all;
+    clc;   
+    curr_res = 100*test_err(maxepoch)/(testnumcases*testnumbatches);
+    res_sum = res_sum + curr_res;
+%     test_array = [ test_array curr_res];
+end
+
+res = res_sum/10;
+fprintf(1,'Result:%.2f %%\n',res);
+
+fid = fopen('experiment_results.csv','a');
+format = '%d;%d;%d;%d;%d;%d;%.2f\n';
+fprintf(fid,format, pretrain_maxepoch, numhid, numpen, numpen2, finetuning_maxepoch, conjugate_gradient_max_iter, res );
+fclose(fid);
+
+end
