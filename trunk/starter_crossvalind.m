5b9e445d02f3f1e96dd0c4dd2f14be6e98352e5e
Giannis21kr@gmail.com <Giannis21kr@gmail.com@d31d3dbd-f071-e4ba-8cac-77037b9f564b>
2014-11-16 22:03:32 +0000

    Run for 200 pretrain epoch and 100 finetune -layers 100 - 400

--- trunk/starter_crossvalind.m
+++ trunk/starter_crossvalind.m
@@ -2,40 +2,41 @@ clc;
 clear all
 close all
 for layer_sizes = 100:100:400
+%% Initialization
+    res_sum = 0;
+    curr_res = 0;
+    % test_array = [];
+    for rounds = 1:10
+%% Setting parameters        
+        pretrain_maxepoch = 300;
+    %     numhid=200; numpen=200; numpen2=200; 
+         numhid=layer_sizes; numpen=layer_sizes; numpen2=layer_sizes; 
+        finetuning_maxepoch = 100;
+        conjugate_gradient_max_iter = 3;
+%% Selecting dataset
+        curr_set = strcat('/set',num2str(rounds, '%02i'));
+        f_train0 = strcat('normalize_softmax',curr_set,'/train0');
+        f_train1 = strcat('normalize_softmax',curr_set,'/train1');
+        f_test0 =  strcat('normalize_softmax',curr_set,'/test0');
+        f_test1 = strcat('normalize_softmax',curr_set,'/test1');
 
-res_sum = 0;
-curr_res = 0;
-% test_array = [];
-for rounds = 1:10
-    pretrain_maxepoch = 100;
-%     numhid=200; numpen=200; numpen2=200; 
-     numhid=layer_sizes; numpen=layer_sizes; numpen2=layer_sizes; 
+%% Running experiment
+        fprintf(1,'Round:%i \n',rounds);
+        classify;
+        close all;
+        clc; 
+%% Storing result from individual run
+        curr_res = 100*test_err(maxepoch)/(testnumcases*testnumbatches);
+        res_sum = res_sum + curr_res;
+    %     test_array = [ test_array curr_res];
+    end
 
-    finetuning_maxepoch = 100;
-    conjugate_gradient_max_iter = 3;
-
-    curr_set = strcat('/set',num2str(rounds, '%02i'));
-    f_train0 = strcat('normalize_softmax',curr_set,'/train0');
-    f_train1 = strcat('normalize_softmax',curr_set,'/train1');
-    f_test0 =  strcat('normalize_softmax',curr_set,'/test0');
-    f_test1 = strcat('normalize_softmax',curr_set,'/test1');
-
-    fprintf(1,'Round:%i \n',rounds);
-
-    classify;
-    close all;
-    clc;   
-    curr_res = 100*test_err(maxepoch)/(testnumcases*testnumbatches);
-    res_sum = res_sum + curr_res;
-%     test_array = [ test_array curr_res];
-end
-
-res = res_sum/10;
-fprintf(1,'Result:%.2f %%\n',res);
-
-fid = fopen('experiment_results.csv','a');
-format = '%d;%d;%d;%d;%d;%d;%.2f\n';
-fprintf(fid,format, pretrain_maxepoch, numhid, numpen, numpen2, finetuning_maxepoch, conjugate_gradient_max_iter, res );
-fclose(fid);
-
+%% Calculating final result 
+    res = res_sum/10;
+    fprintf(1,'Result:%.2f %%\n',res);
+%% Storing result
+    fid = fopen('experiment_results.csv','a');
+    format = '%d;%d;%d;%d;%d;%d;%.2f\n';
+    fprintf(fid,format, pretrain_maxepoch, numhid, numpen, numpen2, finetuning_maxepoch, conjugate_gradient_max_iter, res );
+    fclose(fid);
 end
